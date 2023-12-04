90440a83f6fd7f1102019efc3a6212b4b0f11fa6
Jeroen Stiekema <jeroen@stiekema.net>
2023-12-04 19:56:16 +0100

    day 4, part 2

--- src/main/java/net/stiekema/jeroen/aoc2023/Day4.java
+++ src/main/java/net/stiekema/jeroen/aoc2023/Day4.java
@@ -17,6 +17,8 @@ public class Day4 {
     public static void main(String[] args) throws URISyntaxException, IOException {
         System.out.println("part 1 test: " + calculatePart1Test());
         System.out.println("part 1: " + calculatePart1());
+        System.out.println("part 2 test: " + calculatePart2Test());
+        System.out.println("part 2: " + calculatePart2());
     }
 
     private static long calculatePart1Test() throws URISyntaxException, IOException {
@@ -29,24 +31,52 @@ public class Day4 {
         return calculateTotalScore(resource);
     }
 
+    private static long calculatePart2Test() throws URISyntaxException, IOException {
+        URL resource = Day3.class.getResource("/day4-1-test.txt");
+        return calculateTotalNrOfCards(resource);
+    }
+
+    private static long calculatePart2() throws URISyntaxException, IOException {
+        URL resource = Day3.class.getResource("/day4.txt");
+        return calculateTotalNrOfCards(resource);
+    }
+
     private static long calculateTotalScore(URL resource) throws URISyntaxException, IOException {
         return getLines(resource)
                 .map(Day4::convertToCard)
                 .map(Card::calculateScore)
                 .reduce(Long::sum)
                 .orElse(0L);
+    }
 
+    private static long calculateTotalNrOfCards(URL resource) throws URISyntaxException, IOException {
+        List<Card> cards = getLines(resource)
+                .map(Day4::convertToCard)
+                .collect(Collectors.toList());
 
+        long totalCards = 0;
+        for (int cardId = 0; cardId < cards.size(); cardId++) {
+            totalCards += calculateTotalNrOfCards(cardId, cards);
+        }
+        return totalCards;
+    }
+
+    private static long calculateTotalNrOfCards(int cardId, List<Card> cards) {
+        long winningNumbers = cards.get(cardId).calculateWinningNumbers();
+        long result = 1;
+        for (int i = 1; i <= winningNumbers; i++) {
+            result += calculateTotalNrOfCards(cardId + i, cards);
+        }
+        return result;
     }
 
     private static Card convertToCard(String line) {
         Pattern pattern = Pattern.compile("^Card\\s+([0-9]+):(.*?)\\|(.*?)$");
         Matcher matcher = pattern.matcher(line);
         if (matcher.find()) {
-            int cardNr = Integer.parseInt(matcher.group(1));
             String numbers = matcher.group(2);
             String winningNumbers = matcher.group(3);
-            return new Card(cardNr,
+            return new Card(
                     Arrays.stream(numbers.trim().split("\\s+"))
                             .map(Integer::parseInt)
                             .collect(Collectors.toList()),
@@ -63,13 +93,17 @@ public class Day4 {
         return Files.lines(Paths.get(resource.toURI()), StandardCharsets.UTF_8);
     }
 
-    public record Card (Integer cardNr, List<Integer> numbers, List<Integer> winningNumbers) {
-        public long calculateScore() {
-            long nrOfWinningNumbers = numbers.stream()
+    public record Card (List<Integer> numbers, List<Integer> winningNumbers) {
+
+        public long calculateWinningNumbers() {
+            return numbers.stream()
                     .filter(winningNumbers::contains)
                     .count();
-            long score = nrOfWinningNumbers > 0 ? (long) Math.pow(2, (nrOfWinningNumbers - 1)) : 0;
-            return score;
+        }
+
+        public long calculateScore() {
+            long nrOfWinningNumbers = calculateWinningNumbers();
+            return nrOfWinningNumbers > 0 ? (long) Math.pow(2, (nrOfWinningNumbers - 1)) : 0;
         }
     }
 }
