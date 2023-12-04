6865d1978487db92f434d5426ac491550fbe3fea
Jeroen Stiekema <jeroen@stiekema.net>
2023-12-04 17:53:32 +0100

    day 4

--- src/main/java/net/stiekema/jeroen/aoc2023/Day4.java
+++ src/main/java/net/stiekema/jeroen/aoc2023/Day4.java
@@ -16,16 +16,22 @@ import java.util.stream.Stream;
 public class Day4 {
     public static void main(String[] args) throws URISyntaxException, IOException {
         System.out.println("part 1 test: " + calculatePart1Test());
+        System.out.println("part 1: " + calculatePart1());
     }
 
     private static long calculatePart1Test() throws URISyntaxException, IOException {
-        URL resource = Day3.class.getResource("/day3-1-test.txt");
+        URL resource = Day3.class.getResource("/day4-1-test.txt");
+        return calculateTotalScore(resource);
+    }
+
+    private static long calculatePart1() throws URISyntaxException, IOException {
+        URL resource = Day3.class.getResource("/day4.txt");
         return calculateTotalScore(resource);
     }
 
     private static long calculateTotalScore(URL resource) throws URISyntaxException, IOException {
         return getLines(resource)
-                .map(t -> convertToCard(t))
+                .map(Day4::convertToCard)
                 .map(Card::calculateScore)
                 .reduce(Long::sum)
                 .orElse(0L);
@@ -34,17 +40,17 @@ public class Day4 {
     }
 
     private static Card convertToCard(String line) {
-        Pattern pattern = Pattern.compile("^Card ([0-9]+):(.*?)\\|(.*?)$");
+        Pattern pattern = Pattern.compile("^Card\\s+([0-9]+):(.*?)\\|(.*?)$");
         Matcher matcher = pattern.matcher(line);
         if (matcher.find()) {
             int cardNr = Integer.parseInt(matcher.group(1));
             String numbers = matcher.group(2);
             String winningNumbers = matcher.group(3);
             return new Card(cardNr,
-                    Arrays.stream(numbers.trim().split(" "))
+                    Arrays.stream(numbers.trim().split("\\s+"))
                             .map(Integer::parseInt)
                             .collect(Collectors.toList()),
-                    Arrays.stream(winningNumbers.trim().split(" "))
+                    Arrays.stream(winningNumbers.trim().split("\\s+"))
                             .map(Integer::parseInt)
                             .collect(Collectors.toList())
             );
@@ -62,7 +68,8 @@ public class Day4 {
             long nrOfWinningNumbers = numbers.stream()
                     .filter(winningNumbers::contains)
                     .count();
-            return nrOfWinningNumbers > 0 ? 1 + 2^(nrOfWinningNumbers-1) : 0;
+            long score = nrOfWinningNumbers > 0 ? (long) Math.pow(2, (nrOfWinningNumbers - 1)) : 0;
+            return score;
         }
     }
 }
