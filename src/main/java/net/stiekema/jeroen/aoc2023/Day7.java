94d2ab737e1ea1ff7122b2e15302aa756c2c7841
Jeroen Stiekema <jeroen@stiekema.net>
2023-12-07 21:44:13 +0100

    day 7 part 2

--- src/main/java/net/stiekema/jeroen/aoc2023/Day7.java
+++ src/main/java/net/stiekema/jeroen/aoc2023/Day7.java
@@ -8,30 +8,44 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Day7 {
     public static void main(String[] args) throws URISyntaxException, IOException {
         System.out.println("Part 1 test: " + calculatePart1Test());
         System.out.println("Part 1: " + calculatePart1());
+        System.out.println("Part 2 test: " + calculatePart2Test());
+        System.out.println("Part 2: " + calculatePart2());
     }
 
     private static long calculatePart1Test() throws URISyntaxException, IOException {
         URL resource = Day3.class.getResource("/day7-test.txt");
-        return calculatePart1(resource);
+        return calculate(resource, getPart1Comparator());
     }
 
     private static long calculatePart1() throws URISyntaxException, IOException {
         URL resource = Day3.class.getResource("/day7.txt");
-        return calculatePart1(resource);
+        return calculate(resource, getPart1Comparator());
     }
 
-    private static long calculatePart1(URL resource) throws URISyntaxException, IOException {
+    private static long calculatePart2Test() throws URISyntaxException, IOException {
+        URL resource = Day3.class.getResource("/day7-test.txt");
+        return calculate(resource, getPart2Comparator());
+    }
+
+    private static long calculatePart2() throws URISyntaxException, IOException {
+        URL resource = Day3.class.getResource("/day7.txt");
+        return calculate(resource, getPart2Comparator());
+    }
+
+    private static long calculate(URL resource, Comparator<Hand> comparator) throws URISyntaxException, IOException {
         long result = 0;
         List<Hand> hands = getLines(resource)
                 .map(Day7::parseHand)
-                .sorted()
+                .sorted(comparator)
                 .toList();
         for (int i = 1; i <= hands.size(); i++) {
             Hand hand = hands.get(i-1);
@@ -53,72 +67,89 @@ public class Day7 {
         return Files.lines(Paths.get(resource.toURI()), StandardCharsets.UTF_8);
     }
 
-    private record Hand(List<Card> cards, long bid) implements Comparable<Hand> {
+    private static Comparator<Hand> getPart1Comparator() {
+        return (o1, o2) -> {
+            int compare = Integer.compare(o1.getHandType(), o2.getHandType());
+            if (compare != 0) return compare;
+            return getHandStrengthComparator(Comparator.comparingInt(Card::getStrength)).compare(o1, o2);
+        };
+    }
+
+    private static Comparator<Hand> getPart2Comparator() {
+        return (o1, o2) -> {
+            int compare = Integer.compare(o1.getHandTypeWithJoker(), o2.getHandTypeWithJoker());
+            if (compare != 0) return compare;
+            return getHandStrengthComparator(Comparator.comparingInt(Card::getStrengthWithJoker)).compare(o1, o2);
+        };
+    }
+
+    private static Comparator<Hand> getHandStrengthComparator(Comparator<Card> cardComparator) {
+        return (o1, o2) -> IntStream.range(0, 5)
+                .mapToObj(i -> cardComparator.compare(o1.cards.get(i), o2.cards.get(i)))
+                .filter(i -> i != 0)
+                .findFirst()
+                .orElse(0);
+    }
+
+    private record Hand(List<Card> cards, long bid) {
 
         public int getHandType() {
-            List<Long> cardAmounts = this.cards.stream()
-                    .collect(Collectors.groupingBy(Card::getStrength, Collectors.counting()))
+            List<Integer> cardAmounts = this.cards.stream()
+                    .collect(Collectors.groupingBy(card -> Card.STRENGTH_ORDER.indexOf(card.type), Collectors.counting()))
                     .values().stream()
                     .sorted(Comparator.reverseOrder())
+                    .map(Long::intValue)
                     .toList();
-            if (cardAmounts.get(0) == 5) {
-                return 7;
-            } else if (cardAmounts.get(0) == 4) {
-                return 6;
-            } else if (cardAmounts.size() == 2) {
-                return 5;
-            } else if (cardAmounts.get(0) == 3) {
-                return 4;
-            } else if (cardAmounts.get(0) == 2 && cardAmounts.get(1) == 2) {
-                return 3;
-            } else if (cardAmounts.get(0) == 2) {
-                return 2;
-            } else {
-                return 1;
-            }
+            return getHandType(cardAmounts);
         }
 
-        @Override
-        public int compareTo(Hand o) {
-            int typeCompare = Integer.compare(this.getHandType(), o.getHandType());
-            if (typeCompare != 0) {
-                return typeCompare;
+        public int getHandTypeWithJoker() {
+            int nrOfJokers = (int) this.cards.stream().filter(t -> t.type == 'J').count();
+            AtomicBoolean jokersAdded = new AtomicBoolean(false);
+            List<Integer> cardAmounts;
+            if (nrOfJokers == 5) {
+                cardAmounts = List.of(5);
+            } else {
+                cardAmounts = this.cards.stream()
+                        .filter(t -> t.type != 'J')
+                        .collect(Collectors.groupingBy(card -> Card.STRENGTH_ORDER.indexOf(card.type), Collectors.counting()))
+                        .values().stream()
+                        .sorted(Comparator.reverseOrder())
+                        .map(t -> {
+                            if (jokersAdded.get()) {
+                                return t.intValue();
+                            } else {
+                                jokersAdded.set(true);
+                                return t.intValue() + nrOfJokers;
+                            }
+                        })
+                        .toList();
             }
-            for (int i = 0; i < 5; i++) {
-                int cardCompare = this.cards.get(i).compareTo(o.cards.get(i));
-                if (cardCompare != 0) {
-                    return cardCompare;
-                }
-            }
-            return 0;
+
+            return getHandType(cardAmounts);
+        }
+
+        private static int getHandType(List<Integer> cardAmounts) {
+            return switch (cardAmounts.get(0)) {
+                case 5 -> 7;
+                case 4 -> 6;
+                case 3 -> cardAmounts.get(1) == 2L ? 5 : 4;
+                case 2 -> cardAmounts.get(1) == 2L ? 3 : 2;
+                default -> 1;
+            };
         }
     }
 
-    private record Card(char c) implements Comparable<Card> {
-
-        @Override
-        public int compareTo(Card o) {
-            return Integer.compare(this.getStrength(), o.getStrength());
-        }
+    private record Card(char type) {
+        private static final List<Character> STRENGTH_ORDER = List.of('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A');
+        private static final List<Character> STRENGTH_WITH_JOKER_ORDER = List.of('J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A');
 
         public int getStrength() {
-            if (c >= '2' && c <= '9') {
-                return c - '2';
-            } else {
-                switch (c) {
-                    case 'T':
-                        return 10;
-                    case 'J':
-                        return 11;
-                    case 'Q':
-                        return 12;
-                    case 'K':
-                        return 13;
-                    case 'A':
-                        return 14;
-                }
-            }
-            return 0;
+            return STRENGTH_ORDER.indexOf(type);
+        }
+
+        public int getStrengthWithJoker() {
+            return STRENGTH_WITH_JOKER_ORDER.indexOf(type);
         }
     }
 }
